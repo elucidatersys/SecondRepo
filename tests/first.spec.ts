ee87e5f187bcb64866816ef091bb5fbb5372ac32
Abhinav <abhinav2186@gmail.com>
2026-03-16 20:26:40 +0530

    test case updated to fail

--- tests/first.spec.ts
+++ tests/first.spec.ts
@@ -3,5 +3,6 @@ import { test, expect } from '@playwright/test';
 test('TC1: User able to navigate to the Facebook', async ({ page }) => {
   await page.goto('https://www.facebook.com/');
   console.log("The title of the page is : " + await page.title());
+  await expect(page).toHaveTitle('Facebook - log in or sign ups');
 
 });
