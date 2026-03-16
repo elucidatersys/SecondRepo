c928f143067ed11b9ffd224df769c6f597e66c30
Abhinav <abhinav2186@gmail.com>
2026-03-16 18:43:35 +0530

    Abhinav Workflow Update Commited

--- tests/first.spec.ts
+++ tests/first.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('TC1: User able to navigate to the Facebook', async ({ page }) => {
   await page.goto('https://www.facebook.com/');
-  console.log("The title of the page is : " + page.title());
+  console.log("The title of the page is : " + await page.title());
 
 });
